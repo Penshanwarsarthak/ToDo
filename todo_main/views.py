97bbbd85bea969f320fd303ac80089f71d52d67c
Sarthak Penshanwar <sarthakpenshanwar@Sarthaks-MacBook-Air.local>
2026-01-30 00:52:43 +0530

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    compleated_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
+        'compleated_tasks': compleated_tasks,
     } 
     return render(request, 'home.html', context)
